d7d31e285ed6cfed067c871a9629b63e0e03af91
spark0016 <99815823+spark0016@users.noreply.github.com>
2022-02-17 00:58:37 -0500

    main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+def main():
+    title("I_LOVE_YOU")
+    color("red")
+    begin_fill()
+    pensize(3)
+    left(50)
+    forward(133)
+    circle(50, 200)
+    right(140)
+    circle(50, 200)
+    forward(133)
+    end_fill()
+    mainloop()
+
+if __name__ == "__main__":
+    main()
